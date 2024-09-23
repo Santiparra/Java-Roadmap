da4295b07ce238112d976c04813df8a21c18a3a4
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-23 16:40:51 -0300

    inner classes

--- Basics/AnonymousInnerClasses.java
+++ Basics/AnonymousInnerClasses.java
@@ -5,8 +5,22 @@ Create a class `Animal2` with an abstract method `makeSound`.
 Create an anonymous inner class implementation of `Animal2` and then call the `makeSound` method
  */
 
-public class AnonymousInnerClasses {
-    public static void main(String[] args) {
+ abstract class Animal2 {
+    // Abstract method
+    abstract void makeSound();
+}
 
+public class Main {
+    public static void main(String[] args) {
+        // Anonymous inner class implementation of Animal2
+        Animal2 myAnimal = new Animal2() {
+            @Override
+            void makeSound() {
+                System.out.println("This animal makes a sound.");
+            }
+        };
+
+        // Calling the makeSound method
+        myAnimal.makeSound();
     }
 }
